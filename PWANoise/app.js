0036353bfcb85f788a825a7335fa8d3b5e019889
Squidmond <blightr@msn.com>
2019-02-26 10:25:37 -0600

    Noise Maker Version 0

---
+++ PWANoise/app.js
@@ -0,0 +1,18 @@
+﻿var SoundOn = false;
+document.addEventListener("click", function () {
+    var audioCtx = new (window.AudioContext || window.webkitAudioContext)();
+
+    var oscillator = audioCtx.createOscillator();
+
+
+    if (SoundOn === false) {
+        oscillator.start();
+        oscillator.connect(audioCtx.destination);
+        SoundOn = true;
+    }
+    else {
+        oscillator.stop(audioCtx.currentTime);
+        oscillator.connect(audioCtx.destination);
+        SoundOn = false;
+    }
+});
